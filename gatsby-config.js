2d78a8747a8bd566fa185286c5f7293d4f22d61c
youichiro <cinnamon416@gmail.com>
2019-10-15 19:15:40 +0900

    disable workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
               classPrefix: "language-",
               inlineCodeMarker: null,
               aliases: {},
-              showLineNumbers: true,
+              showLineNumbers: false,
               noInlineHighlight: false,
             },
           },
